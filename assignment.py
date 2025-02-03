e1f4e977503148add130952b274b1ec758a23606
Connor9998 <connorm298@deltalearns.ca>
2025-02-03 10:10:45 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,22 +4,24 @@
 
 
 def title():
-    # Will display a title screen
-    # input parameters: none needed
-    # output parameters: None
-    # Author: Copper
-    # Modified:
-    # title
+    print("calculate your income taxes ._.")
     return None
 
 def instructions():
-    # Will display instructions
-    # input parameters: none needed
-    # output parameters: None
-    # Author:
-    # Modified:
+    print("You will have to tell me what province your in")
+    print("Then, we will have you input your income")
+    print("After that, I will figure what tax bracket your in with my program")      
+    print("Then I will calculate your tax")
+    
     return None
 
+def location():
+    
+
+def taxes:
+bc:[47937,95875,110076,133664,181232,252752,
+alberta:[]
+ontario:[]
 
 
 def main():
@@ -29,6 +31,7 @@ def main():
     the user chooses to exit
     """
     title()
+    instructions()
     while True:
         # keep giving options to choose menu options until they choose to exit
         pass
